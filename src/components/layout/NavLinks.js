1a834ae4d6a60d802cc60e4f924179c0f59da77b
Daiana De Lazzer <dai.d.lazzer@gmail.com>
2022-09-06 16:11:55 -0300

    NavBar hamburger terminado y 100% funcionando

--- src/components/layout/NavLinks.js
+++ src/components/layout/NavLinks.js
@@ -1,13 +1,38 @@
 import { Link } from "react-router-dom";
+import {motion} from 'framer-motion';
 
-const NavLinks = () => {
+
+const NavLinks = (props) => {
+const animateFrom = {opacity: 0, y:-111}
+const animateTo = {opacity: 1, y:0}
+
     return (
         <ul id="Secciones">
-            <li><Link to="/">Inicio</Link></li>
-            <li><Link to="Clases">Clases</Link></li>
-            <li><Link to="Profesores">Profesores</Link></li>
-            <li><Link to="Equipos">Equipos</Link></li>
-            <li><Link to="Novedades">Novedades</Link></li>
+            <motion.li 
+            initial = {animateFrom}
+            animate  = {animateTo}
+            transition ={{delay:0.05}}
+            onClick={() => props.isMenu && props.closeMenu()}><Link to="/" >Inicio</Link></motion.li>
+            <motion.li 
+            initial = {animateFrom}
+            animate  = {animateTo}
+            transition ={{delay:0.10}}
+            onClick={() => props.isMenu && props.closeMenu()}><Link to="Clases">Clases</Link></motion.li>
+            <motion.li
+            initial = {animateFrom}
+            animate  = {animateTo}
+            transition ={{delay:0.15}}
+            onClick={() => props.isMenu && props.closeMenu()}><Link to="Profesores">Profesores</Link></motion.li>
+            <motion.li 
+            initial = {animateFrom}
+            animate  = {animateTo}
+            transition ={{delay:0.20}}
+            onClick={() => props.isMenu && props.closeMenu()}><Link to="Equipos">Equipos</Link></motion.li>
+            <motion.li 
+            initial = {animateFrom}
+            animate  = {animateTo}
+            transition ={{delay:0.25}}
+            onClick={() => props.isMenu && props.closeMenu()}><Link to="Novedades">Novedades</Link></motion.li>
         </ul>
     );
 }
--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -1,15 +1,22 @@
 import NavLinks from './NavLinks';
 import classes from '../../styles/components/layout/header.css';
 import {MdOutlineMenu} from 'react-icons/md'
+import {CgClose} from 'react-icons/cg'
 import { useState } from 'react';
 
 const Navigation = () => {
     const [open, setOpen] = useState(false);
     
+    const hamburgerIcon = <MdOutlineMenu className='Hamburger' size='40px' color='white' onClick={() => setOpen(!open)}></MdOutlineMenu>
+
+    const closeIcon = <CgClose className='Hamburger' size='40px' color='white' onClick={() => setOpen(!open)}></CgClose>
+
+    const closeMenu = () => setOpen(false);
+
     return(
-    <nav className={classes.Navigation}>
-        <MdOutlineMenu className='Hamburger' size='40px' color='white' onClick={() => setOpen(!open)}></MdOutlineMenu>
-        {open && <NavLinks/>}
+    <nav className='Navigation'>
+        {open ? closeIcon : hamburgerIcon}
+        {open && <NavLinks isMenu={true} closeMenu={closeMenu}/>}
      </nav>
     );
 }
