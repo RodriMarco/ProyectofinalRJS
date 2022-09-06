10272f15d7d8adb1c75424efbb19c4df28ef1180
Daiana De Lazzer <dai.d.lazzer@gmail.com>
2022-09-06 15:21:07 -0300

    sguimos desarrollando el nav bar y un poco mas de estetica

--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -1,9 +1,11 @@
 import NavLinks from './NavLinks';
 import classes from '../../styles/components/layout/header.css';
+import {MdOutlineMenu} from 'react-icons/md'
 
 const Navigation = () => {
     return(
     <nav className={classes.Navigation}>
+        <MdOutlineMenu className='Hamburger' size='40px' color='white' onClick={() => console.log("you click")}></MdOutlineMenu>
         <NavLinks/>
      </nav>
     );
