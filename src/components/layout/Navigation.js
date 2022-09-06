55cefd114d557da7eaccd310de2b002dfcd833a1
Daiana De Lazzer <dai.d.lazzer@gmail.com>
2022-09-06 15:31:13 -0300

    continuamos con el sandwich

--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -1,12 +1,15 @@
 import NavLinks from './NavLinks';
 import classes from '../../styles/components/layout/header.css';
 import {MdOutlineMenu} from 'react-icons/md'
+import { useState } from 'react';
 
 const Navigation = () => {
+    const [open, setOpen] = useState(false);
+    
     return(
     <nav className={classes.Navigation}>
-        <MdOutlineMenu className='Hamburger' size='40px' color='white' onClick={() => console.log("you click")}></MdOutlineMenu>
-        <NavLinks/>
+        <MdOutlineMenu className='Hamburger' size='40px' color='white' onClick={() => setOpen(!open)}></MdOutlineMenu>
+        {open && <NavLinks/>}
      </nav>
     );
 }
